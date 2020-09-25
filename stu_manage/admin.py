62245500e080318328b822a21b01db5b77b56685
Sorata <739215370@qq.com>
2020-09-25 23:26:23 +0800

    update database
    rebuilt the tabel

--- stu_manage/admin.py
+++ stu_manage/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 
-from stu_manage.models import Course
+from stu_manage.models import Test
+from stu_manage.models import Examination
 from stu_manage.models import Student
 from stu_manage.models import Score
 
-admin.site.register(Course)
+admin.site.register(Test)
+admin.site.register(Examination)
 admin.site.register(Student)
 admin.site.register(Score)
--- stu_manage/models.py
+++ stu_manage/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Score(models.Model):
     stu_id = models.ForeignKey('Student', on_delete=models.CASCADE)
-    course_id = models.ForeignKey('Course', on_delete=models.CASCADE)
+    test_id = models.ForeignKey('Test', on_delete=models.CASCADE)
     score = models.IntegerField(verbose_name='分数')
 
     def __str__(self):
@@ -24,8 +24,11 @@ class Student(models.Model):
     FEMALE = 'FEMALE'
     GENDER_CHOICE = ((MALE, '男'), (FEMALE, '女'))
     stu_gender = models.CharField(max_length=10, choices=GENDER_CHOICE, verbose_name='性别')
-    GRADE_CHOICE = (('1', '高一'), ('2', '高二'), ('3', '高三'))
-    stu_grade = models.CharField(max_length=10, choices=GENDER_CHOICE, verbose_name='年级')
+    G1 = '高一'
+    G2 = '高二'
+    G3 = '高三'
+    GRADE_CHOICE = ((G1, '高一'), (G2, '高二'), (G3, '高三'))
+    stu_grade = models.CharField(max_length=10, choices=GRADE_CHOICE, verbose_name='年级')
 
     
     # date_added = models.DateTimeField(auto_now_add=True)
@@ -38,7 +41,25 @@ class Student(models.Model):
         verbose_name_plural = '学生名单'
 
 
-class Exam(models.Model):
+
+
+class Test(models.Model):
+    test_id = models.IntegerField(primary_key=True)
+    exam_id = models.ForeignKey('Examination', on_delete=models.CASCADE)
+    S1, S2, S3, S4, S5, S6, S7, S8, S9 = '语文', '数学', '英语', '物理', '化学','生物','历史', '地理', '政治'
+    SUBJECT_CHOICE = ((S1, '语文'), (S2,'数学'), (S3,'英语'), (S4,'物理'), (S5,'化学'), (S6,'生物'), (S7,'历史'), (S8,'地理'), (S9,'政治'))
+    subject = models.CharField(max_length=2, choices=SUBJECT_CHOICE, verbose_name='科目')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = '考试科目'
+        verbose_name_plural = '考试科目'
+
+
+class Examination(models.Model):
+    exam_id = models.IntegerField(primary_key=True)
     exam_name = models.CharField(max_length=15, verbose_name='考试名称')
     exam_date = models.DateField(verbose_name='考试日期')
 
@@ -48,17 +69,3 @@ class Exam(models.Model):
     class Meta:
         verbose_name = '考试列表'
         verbose_name_plural = '考试列表'
-
-
-class Course(models.Model):
-    course_id = models.IntegerField(primary_key=True)
-    exam_name = models.ForeignKey('Exam', on_delete=models.CASCADE)
-    SUBJECT_CHOICE = (('1', '语文'), ('2','数学'), ('3','英语'), ('4','物理'), ('5','化学'), ('6','生物'), ('7','历史'), ('8','地理'), ('9','政治'))
-    subject = models.CharField(max_length=2, choices=SUBJECT_CHOICE, verbose_name='科目')
-
-    def __str__(self):
-        return self.exam_name + self.subject
-
-    class Meta:
-        verbose_name = '考试科目'
-        verbose_name_plural = '考试科目'
